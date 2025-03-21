06c7e15ad522a1d690871aa893f167fec8fa3bb4
mrcichon <mrcvvv@gmail.com>
2025-03-21 23:36:21 +0100

    aight dialog boxes, before merging with json stuff

--- StudySphere/StudySphere.cpp
+++ StudySphere/StudySphere.cpp
@@ -2,6 +2,15 @@
 #include <QInputDialog>
 #include <QMessageBox>
 #include <QVBoxLayout>
+#include <QToolTip>
+#include <QDialog>
+#include <QFormLayout>
+#include <QLineEdit>
+#include <QSpinBox>
+#include <QTextEdit>
+#include <QDialogButtonBox>
+#include <QTimeEdit>
+#include <QCheckBox>
 
 StudySphere::StudySphere(QWidget *parent)
     : QMainWindow(parent)
@@ -11,8 +20,6 @@ StudySphere::StudySphere(QWidget *parent)
    
     
 
-    connect(ui.addExamButton, &QPushButton::clicked, this, &StudySphere::on_addExamButton_clicked);
-    connect(ui.addSubjectButton, &QPushButton::clicked, this, &StudySphere::on_addSubjectButton_clicked);
     connect(ui.backFromInfo, &QPushButton::clicked, this, &StudySphere::on_backFromInfo_clicked);
   
 }
@@ -22,20 +29,107 @@ StudySphere::~StudySphere()
 
 void StudySphere::on_addExamButton_clicked()
 {
-    bool ok;
-    QString examName = QInputDialog::getText(this, "Add Exam", 
-                                         "Exam name:", QLineEdit::Normal,
-                                         "", &ok);
-    if (ok && !examName.isEmpty()) {
-        // Do something with the exam name
-        QMessageBox::information(this, "Success", "Added exam: " + examName);
+    QDate selectedDate = ui.calendarWidget->selectedDate();
+
+    QDialog dialog(this);
+    dialog.setWindowTitle("Add Exam for " + selectedDate.toString("yyyy-MM-dd"));
+
+    QFormLayout formLayout(&dialog);
+
+    QLineEdit *nameEdit = new QLineEdit(&dialog);
+    QLineEdit *roomEdit = new QLineEdit(&dialog);
+    QCheckBox *isRetakeEdit = new QCheckBox(&dialog);
+    QTimeEdit *startTimeEdit = new QTimeEdit(&dialog);
+    QTimeEdit *endTimeEdit = new QTimeEdit(&dialog);
+
+    startTimeEdit->setDisplayFormat("HH:mm");
+    endTimeEdit->setDisplayFormat("HH:mm");
+
+    startTimeEdit->setTime(QTime(8, 0));
+    endTimeEdit->setTime(QTime(9, 0));
+
+    QTextEdit *notesEdit = new QTextEdit(&dialog);
+    notesEdit->setMaximumHeight(100);
+
+    formLayout.addRow("Subject Name:", nameEdit);
+    formLayout.addRow("Room:", roomEdit);
+    formLayout.addRow("Is it a retake:", isRetakeEdit);
+    formLayout.addRow("Start Time:", startTimeEdit);
+    formLayout.addRow("End Time:", endTimeEdit);
+    formLayout.addRow("Notes:", notesEdit);
+
+    QDialogButtonBox buttonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel,
+                              Qt::Horizontal, &dialog);
+    formLayout.addRow(&buttonBox);
+
+    connect(&buttonBox, &QDialogButtonBox::accepted, &dialog, &QDialog::accept);
+    connect(&buttonBox, &QDialogButtonBox::rejected, &dialog, &QDialog::reject);
+
+    if (dialog.exec() == QDialog::Accepted) {
+        QString name = nameEdit->text();
+        QString notes = notesEdit->toPlainText();
+        QTime startTime = startTimeEdit->time();
+        QTime endTime = endTimeEdit->time();
+        QString room = roomEdit->text();
+        QString isRetake = isRetakeEdit->text();
+
+        if (!name.isEmpty()) {
+            QTextCharFormat format;
+            format.setBackground(Qt::cyan);
+            format.setFontWeight(QFont::Bold);
+            ui.calendarWidget->setDateTextFormat(selectedDate, format);
+        } 
     }
-    
 }
+
 void StudySphere::on_addSubjectButton_clicked()
 {
-    QMessageBox::information(this, "Add Subject", "Add Subject button clicked.");
+    QDate selectedDate = ui.calendarWidget->selectedDate();
 
+    QDialog dialog(this);
+    dialog.setWindowTitle("Add Subject for " + selectedDate.toString("yyyy-MM-dd"));
+
+    QFormLayout formLayout(&dialog);
+
+    QLineEdit *nameEdit = new QLineEdit(&dialog);
+    QTimeEdit *startTimeEdit = new QTimeEdit(&dialog);
+    QTimeEdit *endTimeEdit = new QTimeEdit(&dialog);
+
+    startTimeEdit->setDisplayFormat("HH:mm");
+    endTimeEdit->setDisplayFormat("HH:mm");
+
+    startTimeEdit->setTime(QTime(8, 0));
+    endTimeEdit->setTime(QTime(9, 0));
+
+    QTextEdit *notesEdit = new QTextEdit(&dialog);
+    notesEdit->setMaximumHeight(100);
+
+    formLayout.addRow("Subject Name:", nameEdit);
+    formLayout.addRow("Start Time:", startTimeEdit);
+    formLayout.addRow("End Time:", endTimeEdit);
+    formLayout.addRow("Notes:", notesEdit);
+
+    QDialogButtonBox buttonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel,
+                              Qt::Horizontal, &dialog);
+    formLayout.addRow(&buttonBox);
+
+    connect(&buttonBox, &QDialogButtonBox::accepted, &dialog, &QDialog::accept);
+    connect(&buttonBox, &QDialogButtonBox::rejected, &dialog, &QDialog::reject);
+
+    if (dialog.exec() == QDialog::Accepted) {
+        QString name = nameEdit->text();
+        QString notes = notesEdit->toPlainText();
+        QTime startTime = startTimeEdit->time();
+        QTime endTime = endTimeEdit->time();
+
+        if (!name.isEmpty()) {
+            QTextCharFormat format;
+            format.setBackground(Qt::cyan);
+            format.setFontWeight(QFont::Bold);
+            ui.calendarWidget->setDateTextFormat(selectedDate, format);
+
+        }
+    }
 }
 
 void StudySphere::on_backFromInfo_clicked()
